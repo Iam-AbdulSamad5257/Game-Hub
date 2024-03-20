730dcb70bce84a06240c222cd2b92e77f765b7f7
Iam-AbdulSamad5257 <mohdabdulsamad2000@gmail.com>
2024-03-20 16:45:03 +0530

    home page

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <windows.h>
+int main() {
+    char c;
+    system("cls");
+    printf("\t\t\t     ______       ______       ___    ___   ________          \n");
+    printf("\t\t\t   /  _____|     /  __  \\     |   \\__/   | |   _____|         \n");
+    printf("\t\t\t  |  |          /  (__)  \\    |          | |  |___        \n");
+    printf("\t\t\t  |  |   __    /   ____   \\   |  |\\__/|  | |   ___|        \n");
+    printf("\t\t\t  |  |__|  |  /  /      \\  \\  |  |    |  | |  |_____         \n");
+    printf("\t\t\t   \\_______| /__/        \\__\\ |__|    |__| |________|            \n");
+    printf("\n\n");
+    printf("\t\t\t\t   __      __   __      __   _______\n");
+    printf("\t\t\t\t  |  |    |  | |  |    |  | |   __  \\ \n");
+    printf("\t\t\t\t  |  |____|  | |  |    |  | |  |__|  ) \n");
+    printf("\t\t\t\t  |    __    | |  |    |  | |  _____/\n");
+    printf("\t\t\t\t  |   ____   | |  |    |  | |   __  \\\n");
+    printf("\t\t\t\t  |  |    |  | |   \\__/   | |  |__|  )\n");
+    printf("\t\t\t\t  |__|    |__|  \\________/  |_______/\n\n\n\n\n\n");
+    printf("\t\t\t\t      WELCOME   TO   GAME  -  HUB \n\n\n\n\n");
+
+    printf("\t\t\t -------------------------     -------------------------    \n");
+    printf("\t\t\t|  PRESS '1' TO SIGN UP  |     |  PRESS '2' TO SIGN IN  |    \n");
+    printf("\t\t\t -------------------------     -------------------------    \n");
+
+    return 0; 
+}
